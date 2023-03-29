a02fbef2a49c9a771369831783582c24461de4fe
Rob Chizea <chizear@icloud.com>
2023-03-29 11:25:23 +0100

    Lab practice for python

---
+++ rectangle.py
@@ -0,0 +1,5 @@
+length = int(input("Enter rectangle lenghth: "))
+width = int(input("Enter rectangle width: "))
+perimeter = 2 * (width + length)
+area = width * length
+print(f"the perimeter of the rectangle is {perimeter}. The area of the rectangle is {area}.")
