4237c5f4c0e746cc7c65f3481257e11abc495126
Robert Chizea <chizear@icloud.com>
2023-03-30 15:17:05 +0100

    from my own repository

--- ExamGrade2.py
+++ ExamGrade2.py
@@ -3,6 +3,7 @@ examMark = int(input("enter the exam mark: "))
 
 if level < 1 or level > 2:
     print("Level not Valid.")
+    
 elif level == 1:
     if examMark < 1 or examMark > 100:
         print("Error: mark must be between 1 and 100.")
@@ -14,6 +15,7 @@ elif level == 1:
         print("Merit")
     else:
         print("Distinction")
+
 elif level == 2:
     if examMark < 1 or examMark > 100:
         print("Error: mark must be between 1 and 100.")
