3f204d3c04f28ec60012b6a42d991a0432ec2f0f
Robert Chizea <chizear@icloud.com>
2023-05-11 09:59:04 +0100

    further changes

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def hello_internet():
+    return 'Hello Internet!'
+
+@app.route('/name')
 def hello_user():
-    return f'Hello Internet!'
+    name = 'Rob'
+    return f'Hello {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
